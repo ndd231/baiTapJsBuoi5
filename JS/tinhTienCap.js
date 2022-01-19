f792152a1cce2a04938045c88405a46a5c199a20
ndd231 <you@example.com>
2022-01-19 20:09:42 +0700

    bài tập js buổi 5 hoàn chỉnh

--- JS/tinhTienCap.js
+++ JS/tinhTienCap.js
@@ -6,24 +6,34 @@ function checkNhaDan() {
   document.getElementById("soKetNoiThem").style.display = "none";
 }
 
-var txtKenhCaoCap = document.getElementById("soKenhCaoCap").value;
-var txtNhaDan = document.getElementById("nhaDan").value;
-var txtDoanhNghiep = document.getElementById("doanhNghiep").value;
-var tinhCuocCap;
-
 function xuatKetQua() {
-  if (txtNhaDan === "ND") {
-    console.log();
-    tinhCuocCap = 4.5 + 20.5 + txtKenhCaoCap * 7.5;
-  } else {
-    var txtKetNoiThem = document.getElementById("soKetNoiThem").value * 1;
-    console.log(txtKetNoiThem);
-    if (txtKetNoiThem > 10) {
-      tinhCuocCap = 15 + 75 + 5 * (txtKetNoiThem - 10) + 50 * txtKenhCaoCap;
-    } else {
-      tinhCuocCap = 15 + 75 + 50 * txtKenhCaoCap;
-    }
+  var txtKenhCaoCap = document.getElementById("soKenhCaoCap").value;
+  var txtNhaDan = document.getElementById("nhaDan").value;
+  var txtDoanhNghiep = document.getElementById("doanhNghiep").value;
+  var txtKetNoiThem = document.getElementById("soKetNoiThem").value * 1;
+  console.log(txtKetNoiThem);
+  var tinhCuocCap;
+  switch (txtNhaDan) {
+    case "ND":
+      tinhCuocCap = 4.5 + 20.5 + txtKenhCaoCap * 7.5;
+    case "DN":
+      if (txtKetNoiThem > 10) {
+        tinhCuocCap = 15 + 75 + 5 * (txtKetNoiThem - 10) + 50 * txtKenhCaoCap;
+      } else {
+        tinhCuocCap = 15 + 75 + 50 * txtKenhCaoCap;
+      }
   }
+  // if (txtNhaDan === "ND") {
+  //   tinhCuocCap = 4.5 + 20.5 + txtKenhCaoCap * 7.5;
+  // } else {
+  //   var txtKetNoiThem = document.getElementById("soKetNoiThem").value * 1;
+  //   console.log(txtKetNoiThem);
+  //   if (txtKetNoiThem > 10) {
+  //     tinhCuocCap = 15 + 75 + 5 * (txtKetNoiThem - 10) + 50 * txtKenhCaoCap;
+  //   } else {
+  //     tinhCuocCap = 15 + 75 + 50 * txtKenhCaoCap;
+  //   }
+  // }
   document.getElementById("ketQua").innerHTML =
     "Số tiền cước của quý khách là: " + tinhCuocCap + "$";
 }
